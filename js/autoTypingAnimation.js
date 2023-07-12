2aafbef5ff9d83cbece25f7fa97012a1911816cd
Bdnature <84246065+Quddos@users.noreply.github.com>
2023-07-12 10:45:42 -0700

    imported from local drive

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Over 5+ years of Experience in different IT Domain</span> Designing development, troubleshoot, testing and implementation of various stand- alone and client server architecture-based enterprise application software on different domains. <br><br> Working in different Technical industry ,designing different court-edge of technical contribute to multi-International Organization, (Nigeria, India, Usa, Cameroon etc) as Full staff, Intern, Contract and Freelance, also been worked with different vendors in EDUCATIONAL, COMMERCIAL, TECHNOLOGY type of Organization, has helped me to sharpen my skills and confidence to work in any kind of an environment."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
